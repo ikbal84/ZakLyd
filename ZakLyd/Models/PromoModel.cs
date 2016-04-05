15e713a611c4a22edafd58c6e509910c4a332b43
Ikbal <ikba@ikbal.com>
2016-04-05 20:20:34 +0200

    Update Entities

---
+++ ZakLyd/Models/PromoModel.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ZakLyd.Models
+{
+    public class PromoModel
+    {
+        public long PromotionId { get; set; }
+        public long AnnoncementId { get; set; }
+        public long AgencyId { get; set; }
+        public string PromotionName { get; set; }
+        public string PromotionImageUrl { get; set; }
+        public string PromotionImageTitle { get; set; }
+        public float RefPrice { get; set; }
+        public float Price { get; set; }
+        public string PromoText { get; set; }
+        public string AnnouncementLink { get; set; }
+    }
+}
